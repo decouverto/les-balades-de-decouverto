c6f5e7d5a478158181767d82cc71eeda36004c9e
Cédric JUNG <cedced19@gmail.com>
2018-05-23 19:18:20 +0200

    Add button to share position

--- app/components/sidebar/sidebar.js
+++ app/components/sidebar/sidebar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { AppRegistry, Image, StatusBar } from 'react-native';
+import { AppRegistry, Image, StatusBar, Alert, Share } from 'react-native';
 import { Container, Content, Text, List, ListItem, View, Icon } from 'native-base';
 const routes = [{
     way: 'Home', text: 'Accueil', icon: 'ios-home'
@@ -33,9 +33,34 @@ export default class SideBar extends React.Component {
                                     onPress={() => this.props.navigation.navigate(data.way)}>
                                     <Icon name={data.icon} /><Text> {data.text}</Text>
                                 </ListItem>
+
                             );
                         }}
                     />
+                    <ListItem
+                        button
+                        onPress={() => {
+                            navigator.geolocation.getCurrentPosition((location) => {
+                                var date = new Date();
+                                date.setTime(location.timestamp);
+                                Share.share(
+                                    {
+                                        message: `Je suis actuellement aux coordonnées GPS suivante: ${location.coords.latitude} ${location.coords.longitude} https://www.google.fr/maps/place/${location.coords.latitude}+${location.coords.longitude} (position mesurée à ${date.getHours() < 10 ? '0' : ''}${date.getHours()}:${date.getMinutes() < 10 ? '0' : ''}${date.getMinutes()})`
+                                    }
+                                );
+                            }, () => {
+                                Alert.alert(
+                                    'Erreur',
+                                    'Impossible de vous géolocaliser.',
+                                    [
+                                        { text: 'Ok' },
+                                    ]
+                                );
+                            }, { enableHighAccuracy: true });
+                        }}>
+                        <Icon name='ios-pin' /><Text> Partager ma position</Text>
+                    </ListItem>
+
                 </Content>
             </Container>
         );
